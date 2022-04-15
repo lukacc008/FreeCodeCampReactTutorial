d37b7bede1e65d9b6c6d654d58afb22ca34696b3
unknown <lukacc008@gmail.com>
2022-04-15 16:44:33 +0200

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-ReactDOM.render (<h1>Hello, Everyone</h1>, document.getElementById("root"));
+//ReactDOM.render (<h1 className="header">Hello, React</h1>, document.getElementById("root"));
+
+/*
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
+*/
+
+//JSX
+/*
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+*/
+
+const Navigacija = (
+    <nav>
+        <h1>Manijak.com</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    
+)
+
+
+
+ReactDOM.render(
+    Navigacija,
+     document.getElementById("root")
+     );
